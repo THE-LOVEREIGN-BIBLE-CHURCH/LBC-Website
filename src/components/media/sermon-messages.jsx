4dc76a8ed41cc3ecb251bf0a308c46ee369383b8
Wisdom Eli <elwisdom005@gmail.com>
2025-05-04 17:40:36 +0000

    links

--- src/components/media/sermon-messages.jsx
+++ src/components/media/sermon-messages.jsx
@@ -206,7 +206,7 @@ export default function SermonMessages() {
 
                 <div className="mt-12 text-center">
                     <a
-                        href={activeTab === "podbean" ? "https://podbean.com" : "https://youtube.com"}
+                        href={activeTab === "podbean" ? "https://lovereignbiblechurch.podbean.com/" : "https://www.youtube.com/@lovereignbiblechurch}
                         target="_blank"
                         rel="noopener noreferrer"
                         className={`inline-flex items-center justify-center px-6 py-2 text-sm font-medium transition-all border ${
